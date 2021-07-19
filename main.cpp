304fdc9e70c19c1756e4b23dcbfe4f44bf147e5c
Piyush Saxena <piyushsaxena666@gmail.com>
2021-07-19 20:13:28 +0530

    added booked function and deq

--- main.cpp
+++ main.cpp
@@ -10,92 +10,7 @@
 
 using namespace std;
 
-unordered_map<string,vector<Flight>> graph;
 
-class Passenger {
-
-	char name[20];
-	char password[20];
-	char aadharNo[20];
-	char phone[10];
-	char address[40];
-	char passportNo[20];
-	char dob[10];
-	int history[15];
-	int n;
-	int discount;
-public:
-//	Passenger(char name[20],char aadharNo[20],char phone[10],char address[40],char passportNo[20],char dob[10])
-//	{
-//        strcpy(this->name,name);
-//        strcpy(this->aadharNo,aadharNo);
-//        strcpy(this->phone,phone);
-//        strcpy(this->address,address);
-//        strcpy(this->passportNo,passportNo);
-//        strcpy(this->dob,dob);
-//	}
-
-	Passenger()
-	{
-	    n=0;
-    }
-
-	void input()
-	{
-	    cout<<"Enter Name : ";cin>>name;
-	    cout<<"Password :";cin>>password;
-	    cout<<"Aadhar Number : ";cin>>aadharNo;
-	    cout<<"Phone : ";cin>>phone;
-	    cout<<"Address : ";cin>>address;
-	    cout<<"Passport Number : ";cin>>passportNo;
-	    cout<<"Date Of Birth : ";cin>>dob;
-	}
-
-	void output()
-	{
-        cout<<"\nName : "<<name;
-	    cout<<"\nAadhar Number : "<<aadharNo;
-	    cout<<"\nPhone : "<<phone;
-	    cout<<"\nAddress : "<<address;
-	    cout<<"\nPassport Number : "<<passportNo;
-	    cout<<"\nDate Of Birth : "<<dob;
-	}
-
-	bool checkPass(char password[20])
-	{
-	    return strcmp(this->password,password);
-	}
-
-	bool checkName(char name[20])
-	{
-	    return strcmp(this->name,name);
-	}
-	void book()
-	{   string src,dest;
-	    char x;
-	    cout<<"Enter Source City : ";
-	    cin>>src;
-	    cout<<"Enter Destination City : ";
-	    cin>>dest;
-	    cout<<"What matters more to you Time or Money?(Press t for Time and m for Money) : \n";
-
-	    do()
-	    { cin>>x;
-	        if(x=='t')
-        {
-            ShortestPathTime(src,dest);
-        }
-        else if(x=='m')
-        {
-            ShortestPathCost(src,dest);
-        }
-        else
-        {
-            cout<<"It looks like enjoyment matters more to you.(Press either t or m)"<<'\n';
-        }
-	    }while(x=='t'||x=='m')
-	}
-};
 
 class Flight {
 	char flightName[20];
@@ -168,6 +83,148 @@ public:
 	    return duration;
 	}
 };
+unordered_map<string,vector<Flight>> graph;
+pair<int,vector<Flight>> shortestPathCost(string src,string dest);
+pair<int,vector<Flight>> shortestPathTime(string src,string dest);
+
+void dInsert(int arr[],int &n,int &s,int data)
+{
+    if(n!=15)
+        n++;
+    arr[(s+1)%n]=data;
+    s=(s+1)%n;
+
+}
+
+void dPrint(int arr[],int &s,int &n)
+{
+    for(int i=s;i>=0;i--)cout<<arr[i]<<" ";
+    for(int i=n-1;i>s;i--)cout<<arr[i]<<" ";
+}
+
+class Passenger {
+
+	char name[20];
+	char password[20];
+	char aadharNo[20];
+	char phone[10];
+	char address[40];
+	char passportNo[20];
+	char dob[10];
+	int history[15];
+	int n;
+	int s; // deque start pointer
+	int discount;
+public:
+//	Passenger(char name[20],char aadharNo[20],char phone[10],char address[40],char passportNo[20],char dob[10])
+//	{
+//        strcpy(this->name,name);
+//        strcpy(this->aadharNo,aadharNo);
+//        strcpy(this->phone,phone);
+//        strcpy(this->address,address);
+//        strcpy(this->passportNo,passportNo);
+//        strcpy(this->dob,dob);
+//	}
+
+	Passenger()
+	{
+	    n=0;
+	    discount=0;
+	    s=-1;
+    }
+
+	void input()
+	{
+	    cout<<"Enter Name : ";cin>>name;
+	    cout<<"Password :";cin>>password;
+	    cout<<"Aadhar Number : ";cin>>aadharNo;
+	    cout<<"Phone : ";cin>>phone;
+	    cout<<"Address : ";cin>>address;
+	    cout<<"Passport Number : ";cin>>passportNo;
+	    cout<<"Date Of Birth : ";cin>>dob;
+	}
+
+	void output()
+	{
+        cout<<"\nName : "<<name;
+	    cout<<"\nAadhar Number : "<<aadharNo;
+	    cout<<"\nPhone : "<<phone;
+	    cout<<"\nAddress : "<<address;
+	    cout<<"\nPassport Number : "<<passportNo;
+	    cout<<"\nDate Of Birth : "<<dob;
+	}
+
+	bool checkPass(char password[20])
+	{
+	    return strcmp(this->password,password);
+	}
+
+	bool checkName(char name[20])
+	{
+	    return strcmp(this->name,name);
+	}
+	void book()
+	{   string src,dest;
+	    char x;
+	    cout<<"Enter Source City : ";
+	    cin>>src;
+	    cout<<"Enter Destination City : ";
+	    cin>>dest;
+	    cout<<"What matters more to you Time or Money?(Press t for Time and m for Money) : \n";
+        pair<int,vector<Flight>>minPath;
+        int redeemed=0;
+	    do
+	    {   cin>>x;
+	        if(x=='t')
+            {
+                minPath=shortestPathTime(src,dest);
+            }
+            else if(x=='m')
+            {
+                minPath=shortestPathCost(src,dest);
+            }
+            else
+            {
+                cout<<"It looks like enjoyment matters more to you.(Press either t or m)"<<'\n';
+            }
+	    }while(x=='t'||x=='m');
+	    auto booked=[&](){
+            cout<<"\nWooHoo! You are all set to go\n";
+            cout<<"Congratulations! You have received a discount coupon of "<<minPath.first*0.05;
+            discount+=minPath.first*0.05;
+            for(auto &i:minPath.second){
+                dInsert(history,n,s,i.getFlightNo());
+            }
+	    };
+	    if(minPath.first!=-1){
+            for(auto &i:minPath.second)
+                i.output();
+            cout<<"Do You Want To Book These Flights (y/n) : ";
+            char c;
+            cin>>c;
+            if(c=='y' || 'Y'){
+                if(!discount){
+                    booked();
+                }
+                else{
+                    cout<<"Do You Want To Redeem Your Discount Coupon (y/n) : ";
+                    char ch;
+                    cin>>ch;
+                    if(ch=='y'){
+                        redeemed=discount;
+                        discount=0;
+                        booked();
+                    }
+                    else booked();
+                }
+            }
+	    }
+
+
+	}
+};
+
+
 
 bool login(Passenger&p,fstream&file)
 {
@@ -223,7 +280,7 @@ void signup(fstream&file){
 //    cout<<"Disconnected Components : "<<k<<endl;
 //}
 
-int shortestPath(unordered_map<string,vector<Flight>>&graph,string src,string dest)
+pair<int,vector<Flight>> shortestPathCost(unordered_map<string,vector<Flight>>&graph,string src,string dest)
 {
     unordered_map<string,int>dist;
     unordered_map<string,Flight>parent;
@@ -251,20 +308,56 @@ int shortestPath(unordered_map<string,vector<Flight>>&graph,string src,string de
         }
 
     }
-    stack<Flight>st;
+   vector<Flight>st;
     string temp=dest;
     while(parent[dest].getFrom()!=src){
-        st.push(parent[dest]);
+        st.push_back(parent[dest]);
         dest=parent[dest].getFrom();
     }
-    st.push(parent[dest]);
-    while(!st.empty()){
-        st.top().output();cout<<endl;
-        st.pop();
-    }
-    return dist[temp];
+    st.push_back(parent[dest]);
+    reverse(st.begin(),st.end());
+
+    return {dist[temp],st};
 }
 
+pair<int,vector<Flight>> shortestPathTime(unordered_map<string,vector<Flight>>&graph,string src,string dest)
+{
+    unordered_map<string,int>dist;
+    unordered_map<string,Flight>parent;
+    dist[src]=0;
+    set<pair<int,string>>s;
+    s.insert({0,src});
+    while(!s.empty())
+    {
+        pair<int,string>t=*(s.begin());
+        s.erase(s.begin());
+        string fromCity=t.second;
+        for(auto&f:graph[t.second])
+        {
+            string toCity=f.getTo();
+            if(dist[toCity]==0)dist[toCity]=INT_MAX;
+
+            if(dist[toCity]>dist[fromCity]+f.getDuration()){
+                 if(s.find({dist[toCity],toCity})!=s.end()){
+                     s.erase({dist[toCity],toCity});
+                 }
+                 dist[toCity]=dist[fromCity]+f.getDuration();
+                 s.insert({dist[toCity],toCity});
+                 parent[toCity]=f;
+            }
+        }
+
+    }
+    vector<Flight>st;
+    string temp=dest;
+    while(parent[dest].getFrom()!=src){
+        st.push_back(parent[dest]);
+        dest=parent[dest].getFrom();
+    }
+    st.push_back(parent[dest]);
+    reverse(st.begin(),st.end());
+    return {dist[temp],st};
+}
 
 int main()
 {
@@ -287,7 +380,7 @@ int main()
 //        cout<<endl<<endl;
 //    }
 
-//    cout<<shortestPath(graph,"Karachi","Chicago");
+//    cout<<shortestPath(graph,"Sydney","Chicago");
 
 //    dfs_util(graph);
     file.open("passenger.dat",ios::app|ios::in|ios::binary);
